a94bd952955fa277467a4d3f4b5ec5a348f8fa90
areeb193 <fa22-bse-193@cuilahore.edu.pk>
2025-07-29 19:27:18 +0500

    3:00:53 done

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,7 +4,7 @@ test ("Valid Login", async function({page}) {
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     
     await page.getByPlaceholder("Username").type("Admin",{delay: 100})
-    await page.locator("input[placeholder='Password']").type("admin123",{delay: 100})
+    await page.locator("input[placeholder='Password']").type("admin1234",{delay: 100})
     await page.locator("//button[@type='submit']").click()
     await page.waitForTimeout(5000); // Wait for 2 seconds to ensure the page loads
     // Verify that the URL contains 'dashboard' after login
@@ -12,6 +12,6 @@ test ("Valid Login", async function({page}) {
     await page.getByAltText("profile picture").first().click();
 
     await page.getByText("Logout").click();
-    await page.waitForTimeout(5000);
+   // await page.waitForTimeout(5000);
     await expect(page).toHaveURL(/login/);
 })
